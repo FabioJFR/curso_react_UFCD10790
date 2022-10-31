41635d3ed71f96ab8f95100c0361528d8290ed5d
FabioJFR <fabiojfrevez@gmail.com>
2022-10-31 22:20:40 +0000

    Update App.js

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Formação : Feedback App
         </a>
       </header>
     </div>
